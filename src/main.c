c1835f4f64aaf5e68d8dbfc61fcd28a57efdbf6c
Rodrigo Kobayashi <kobayashi.kenji.r@gmail.com>
2024-04-18 13:16:57 -0400

    atualiza aloca e get_key

--- src/main.c
+++ src/main.c
@@ -16,6 +16,24 @@ typedef struct{
     char fuso_horario[30];
 }tmunicipio;
 
+char *get_key(void *reg) {
+    return ((tmunicipio *)reg)->codigo_ibge;
+}
+
+void *aloca_municipio(char *codigo_ibge, char *nome, float latitude, float longitude, int capital, int codigo_uf, int siafi_id, int ddd, char *fuso_horario) {
+    tmunicipio *municipio = malloc(sizeof(tmunicipio));
+    strcpy(municipio->codigo_ibge, codigo_ibge);
+    strcpy(municipio->nome, nome);
+    municipio->latitude = latitude;
+    municipio->longitude = longitude;
+    municipio->capital = capital;
+    municipio->codigo_uf = codigo_uf;
+    municipio->siafi_id = siafi_id;
+    municipio->ddd = ddd;
+    strcpy(municipio->fuso_horario, fuso_horario);
+    return municipio;
+}
+
 int main(){
 
 
